1b6f3b3b24224a27c19fff2691e3b628a18e539f
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-28 04:06:56 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
-    erb :'users/show'
+    erb :"/users/show"
   end
 
   get '/signup' do
