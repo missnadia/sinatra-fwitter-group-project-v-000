2779c3ddb9d274cc02bb222efe7b12a277c3c0ff
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-28 03:34:27 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -38,7 +38,11 @@ class TweetsController < ApplicationController
   get '/tweets/:id/edit' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      erb :"/tweets/edit_tweet"
+      if @tweet.user == current_user
+        erb :"/tweets/edit_tweet"
+      else
+        redirect to "/tweets"
+      end
     else
       redirect "/login"
     end
