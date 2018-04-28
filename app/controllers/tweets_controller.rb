7d5d881331dddfe4757524213673784c8059d0b6
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-28 04:41:29 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -25,7 +25,7 @@ class TweetsController < ApplicationController
       redirect to '/login'
     end
   end
-
+binding.pry
   get '/tweets/:id' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
