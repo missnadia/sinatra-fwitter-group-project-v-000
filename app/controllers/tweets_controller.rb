34ca46b55787d17df220915ea37f92250b8dd82e
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-28 04:45:18 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -68,6 +68,7 @@ class TweetsController < ApplicationController
   end
 
   delete '/tweets/:id/delete' do
+    binding.pry
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
       if @tweet.user == current_user
