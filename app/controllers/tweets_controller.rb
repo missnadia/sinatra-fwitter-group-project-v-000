a52230b5131a7511c294f5d7a27f2de4871fe959
Nadia Lee <miss.nadia.lee@gmail.com>
2018-04-28 04:55:23 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -60,9 +60,7 @@ class TweetsController < ApplicationController
   delete '/tweets/:id/delete' do
     if logged_in?
       @tweet = Tweet.find_by_id(params[:id])
-      if @tweet.user == current_user
-        @tweet.delete
-      end
+      @tweet.delete if @tweet.user == current_user
     else
       redirect to "/login"
     end
